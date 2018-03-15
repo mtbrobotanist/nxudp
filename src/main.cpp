ccb9e928475a29c61105a2f399e70e8d2c57f71a
Unknown <floydwin7@gmail.com>
2018-03-15 01:00:18 -0700

    added help text

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include "asio.hpp"
+#include <asio.hpp>
 
+
+void help()
+{
+    std::string help = "Usage:\n";
+    help += "\n";
+    help += "   Client Mode\n";
+    help += "       app -c <ip-address>:<port> -n <milliseconds>\n";
+    help += "\n";
+    help += "   ServerMode\n";
+    help += "       app -s\n";
+
+    std::cout << help << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "testing" << std::endl;
+    if(argc == 1)
+    {
+        help();
+        return 0;
+    }
+
+    std::cout << "argc: " << argc << std::endl;
+    std::cout << "argv: " << argv[0] << std::endl;
     return 0;
 }
