783ebbd0f1bf2495cbf31fb22cd737c46ef54d01
Unknown <floydwin7@gmail.com>
2018-03-15 00:19:06 -0700

    Finished initial project setup

---
+++ cmake/Modules/Findasio.cmake
@@ -0,0 +1,16 @@
+find_path( ASIO_INCLUDE_DIRS asio.hpp
+        HINTS
+            "${PROJECT_SOURCE_DIR}/vendor/asio/asio/include"
+            "/usr/include"
+            "/usr/local/include"
+            "/opt/local/include"
+            "C:/asio")
+
+if ( ASIO_INCLUDE_DIRS )
+    set( ASIO_FOUND TRUE )
+    set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DASIO_STANDALONE=YES" )
+
+    message( STATUS "${Green}Found ASIO include at: ${ASIO_INCLUDE_DIRS}.${Reset}" )
+else ( )
+    message( FATAL_ERROR "${Red}Failed to locate ASIO dependency.${Reset}" )
+endif ( )
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "asio.hpp"
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "testing" << std::endl;
+    return 0;
+}
