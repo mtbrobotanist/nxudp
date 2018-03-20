723dadb749fd7da06c271060d65fb299ad68eb09
Unknown <floydwin7@gmail.com>
2018-03-20 00:21:22 -0700

    fixed client.h copyright header

--- src/client.h
+++ src/client.h
@@ -3,13 +3,8 @@
 //
 
 //
-// client.cpp
+// client.h
 // ~~~~~~~~~~
-//
-// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.std.org/LICENSE_1_0.txt)
 //
 
 #include <iostream>
