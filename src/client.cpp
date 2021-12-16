1e066809aa7944721ea448665bddf5b315da3546
n8.wilson <floydwin7@gmail.com>
2021-12-16 11:13:50 -0800

    simplify client

--- src/client.cpp
+++ src/client.cpp
@@ -13,12 +13,13 @@ namespace nxudp
 using asio::ip::udp;
 
 client::client(asio::io_service& io, const asio::ip::udp::endpoint& server_endpoint, int timeout) :
-    network_object(io, server_endpoint),
-    _timeout(timeout)
+    _socket(io, udp::endpoint(udp::v4(), 0)),
+    _server_endpoint(server_endpoint),
+    _timeout(timeout),
+    _message_buffer(4, 0) //< "DONE"
 {
     connect_to_server();
 }
-
 
 client::~client()
 {
@@ -27,93 +28,50 @@ client::~client()
 
 void client::connect_to_server()
 {
-    auto func = std::bind(&client::async_connect_callback, this, std::placeholders::_1);
-
-    _socket.async_connect(server_endpoint(), func);
+    _socket.async_connect(
+        _server_endpoint,
+        [this](const asio::error_code &error)
+        {
+            if(error)
+                print_stream(std::cerr) << "Error connecting to server: " << error << "\n";
+            else
+                send_timeout();
+        });
 }
 
 void client::send_timeout()
 {
-    auto func = std::bind(&client::async_send_callback, this,
-                          std::placeholders::_1,
-                          std::placeholders::_2);
+    nxudp::utils::write_buffer(_timeout, _timeout_buffer);
 
-    nxudp::utils::write_buffer(timeout(), _timeout_buffer);
+    _socket.async_send(
+        asio::buffer(_timeout_buffer),
+        [this](const asio::error_code &error, std::size_t)
+        {
+            if(error)
+            {
+                print_stream(std::cerr) << "Error sending: " << error << "\n";
+            }
+            else
+            {
+                print_stream() << "Sent datagram with value \"" << _timeout << "\" to " << _server_endpoint
+                        << ".\nWaiting for response...\n";
 
-   _socket.async_send(asio::buffer(_timeout_buffer), func);
+                wait();
+            }
+        });
 }
 
 void client::wait()
 {
-    auto func = std::bind(&client::async_receive_callback, this,
-                          std::placeholders::_1,
-                          std::placeholders::_2);
-
-    _socket.async_receive(asio::buffer(_message_buffer), func);
-}
-
-void client::async_connect_callback(const asio::error_code &error)
-{
-    if(error)
-    {
-        print_stream(std::cerr) << "Error connecting to server: " << error << "\n";
-    }
-    else
-    {
-        send_timeout();
-    }
-}
-
-void client::async_send_callback(const asio::error_code &error, std::size_t)
-{
-    if(error)
-    {
-        print_stream(std::cerr) << "Error sending: " << error << "\n";
-    }
-    else
-    {
-        print_stream() << "Sent datagram with value \"" << timeout() << "\" to " << server_endpoint()
-                  << ".\nWaiting for response...\n";
-
-        wait();
-    }
-}
-
-void client::async_receive_callback(const asio::error_code &error, std::size_t bytes_transferred)
-{
-    if (error)
-    {
-        print_stream(std::cerr) << "Error receiving:" << error << "\n";
-    }
-    else
-    {
-        std::string message;
-        parse_response(_message_buffer, bytes_transferred, message);
-        print_stream() << "Received response \"" << message << "\" from " << server_endpoint() << ".\n";
-    }
-
-}
-
-void client::parse_response(const client::message_buffer &buffer,
-                            std::size_t bytes_transferred,
-                            std::string& out_message)
-{
-    out_message.reserve(bytes_transferred);
-
-    for(std::size_t i = 0; i < bytes_transferred; ++i)
-    {
-        out_message.push_back(buffer[i]);
-    }
-}
-
-const asio::ip::udp::endpoint& client::server_endpoint() const
-{
-    return _remote_endpoint;
-}
-
-int client::timeout() const
-{
-    return _timeout;
+    _socket.async_receive(
+        asio::buffer(_message_buffer),
+        [this](const asio::error_code &error, std::size_t bytes_transferred)
+        {
+            if (error)
+                print_stream(std::cerr) << "Error receiving:" << error << "\n";
+            else
+                print_stream() << "Received response \"" << _message_buffer << "\" from " << _server_endpoint << ".\n";
+        });
 }
 
 }// namespace nxudp
--- src/client.h
+++ src/client.h
@@ -9,22 +9,22 @@
 
 #include <iostream>
 #include <array>
+#include "asio/io_service.hpp"
+#include "asio/ip/udp.hpp"
 #include "int_buffer.h"
 #include "export.h"
-#include "network_object.h"
 
 namespace nxudp
 {
 
 /// Represents a client when the app is run in client mode. This class sends a timeout, in milliseconds, to the server.
 /// The server then waits the specfied amount of time before replying back to the client.
-class NXUDP_API client : network_object
+class NXUDP_API client
 {
 
 public:
 
     typedef int_buffer send_buffer;
-    typedef std::array<char, 128> message_buffer;
 
 public:
     /// Constructs a client that will send the timeout.
@@ -34,10 +34,9 @@ public:
     /// This is the timeout sent out on the socket to the server.
     client(asio::io_service& io, const asio::ip::udp::endpoint&server_endpoint, int timeout);
 
-    virtual ~client();
+    ~client();
 
 private:
-
     /// connects the _socket to the server_endpoint()
     void connect_to_server();
 
@@ -47,38 +46,16 @@ private:
     /// Calls the socket's async_receive_from() function;
     void wait();
 
-    /// The callback function called by asio when _socket.async_send_to completes..
-    /// @param[in] error - an error code if one occured, provided by asio.
-    /// @param[in] bytes_transferred - the number of bytes sent out on the _socket, provided by asio.
-    void async_send_callback(const asio::error_code &error, std::size_t bytes_transferred);
-
-    /// The callback function called by asio when _socket.async_receive_from() completes.
-    /// @param[in] error - an error code if one occured, provided by asio.
-    /// @param[in] bytes_transferred - the number of bytes sent out on the _socket, provided by asio.
-    void async_receive_callback(const asio::error_code &error, std::size_t bytes_transferred);
-
-    /// The callback function called by asiuon when _socket.async_connect() completes.
-    /// @param[in] error - the error code in one occured.
-    void async_connect_callback(const asio::error_code &error);
-
-    /// A helper function that converts the contents of the given buffer to a string.
-    /// @param[in] buffer - the buffer whose contents to convert, typically filled in by the socket itself.
-    /// @param[in] bytes_transferred - the number of bytes transferred to the socket.
-    /// @param[out] out_message - the string that will be assigned the parsed contents of the buffer
-    void parse_response(const message_buffer &buffer, std::size_t bytes_transferred,
-                        std::string& out_message);
-
-    /// get the endpoint of the server this client is connected to.
-    const asio::ip::udp::endpoint& server_endpoint() const;
-
     /// get the time out for this client
     int timeout() const;
 
 private:
-
+    asio::ip::udp::socket _socket;
+    asio::ip::udp::endpoint _server_endpoint;
     int _timeout;
-    message_buffer _message_buffer;
 
+    int_buffer _timeout_buffer;
+    std::string _message_buffer;
 };
 
 }// namespace nxudp
